426fd885234a72e8580f49eecaafb79b80699d7f
Shely <makaxsole@gmail.com>
2019-06-15 22:51:17 -0700

    adding bootstrap, adding content

--- src/App.tsx
+++ src/App.tsx
@@ -25,14 +25,47 @@ const App: React.FC = () => {
           problem is caused by a general lack of awareness of the impact that
           food waste has on the environment.
         </p>
-        <p>
+        <div>
           Join us in our efforts to help raise awareness about food waste and
           the monetary benefits that come along with reducing food waste in the
           Greater Vancouver area.
-        </p>
+        </div>
+        <div className="row">
+          <div className="col-3">
+            <img />
+            <h3>Plenty of Thyme</h3>
+            <div>
+              Its in the name! Keep track of your food's expiry dates with our
+              reminders. Our calendar views will keep you posted on your food's
+              life.
+            </div>
+          </div>
+          <div className="col-3">
+            <img />
+            <h3>Save food</h3>
+            <div>
+              Keep track of all the food in your kitchen while reducing food
+              waste. Help save the world!
+            </div>
+          </div>
+          <div className="col-3">
+            <img />
+            <h3>Keep you money</h3>
+            <div>
+              Stay up to date with your finances as we show how much money you
+              could potentially be saving!
+            </div>
+          </div>
+        </div>
         <img alt="Video" />
       </div>
-      <div className="">Why use this application</div>
+      <div className="">
+        <h1>Why Use Plenty of Thyme?</h1>
+        <p>
+          Plenty of Thyme is a web based grocery application that keeps track of
+          items currently in your fridge as well as their expiry dates.
+        </p>
+      </div>
       <div className="">About the Author</div>
       <div className="">Our Freinds split screen</div>
       <div className="">Affiliates</div>
