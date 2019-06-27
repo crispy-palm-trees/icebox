dfdc5d7cde4939e4590368db9eab8b6c8a48e32b
Shely <makaxsole@gmail.com>
2019-06-27 12:06:23 -0700

    Updates

---
+++ precache-manifest.8ec7921cb4f86ae714348ce71dd6d78e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c74bb85593aeaf671c84be4d61742a2b",
+    "url": "/icebox/index.html"
+  },
+  {
+    "revision": "9b2df0b2516db804ed71",
+    "url": "/icebox/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "0a97b01ab3899e1512dc",
+    "url": "/icebox/static/css/main.903b8a1e.chunk.css"
+  },
+  {
+    "revision": "9b2df0b2516db804ed71",
+    "url": "/icebox/static/js/2.8b46ad5d.chunk.js"
+  },
+  {
+    "revision": "0a97b01ab3899e1512dc",
+    "url": "/icebox/static/js/main.f488becf.chunk.js"
+  },
+  {
+    "revision": "05e43ee518d3e2625802",
+    "url": "/icebox/static/js/runtime~main.bed20d08.js"
+  },
+  {
+    "revision": "842ce12378ebb55288b780638a4bc9c0",
+    "url": "/icebox/static/media/banner2.842ce123.jpg"
+  },
+  {
+    "revision": "32ac01c8c925a2587a000d931ba91586",
+    "url": "/icebox/static/media/book.32ac01c8.png"
+  },
+  {
+    "revision": "5fcffac1ab324bf23aef52e0639a5b8f",
+    "url": "/icebox/static/media/goose.5fcffac1.jpg"
+  },
+  {
+    "revision": "de5432d0ac20412b307ddbfe6d38509b",
+    "url": "/icebox/static/media/swan.de5432d0.jpg"
+  }
+]);
